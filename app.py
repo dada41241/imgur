e9b0b5e6cd727e11cf8cb5e563fe6a5d6a24e053
dada41241 <32788226+dada41241@users.noreply.github.com>
2020-05-22 11:46:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,75 +1,91 @@
-#from _future_ import print_function
-#from apiclient.discovery import build
-#from httplib2 import Http
-#from oauth2client import file, client, tools
-
-#import time
-#import re
-#import datetime
-#import random
-#import codecs
-#import sys
-#import json
-
 from flask import Flask, request, abort
-from urllib.request import urlopen
-#from oauth2client.service_account import ServiceAccountCredentials
+
+
 
 from linebot import (
+
     LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError,LineBotApiError
+
 )
 
-################################
+from linebot.exceptions import (
+
+    InvalidSignatureError
+
+)
 
 from linebot.models import *
 
+
+
 app = Flask(__name__)
 
+
+
 # Channel Access Token
-line_bot_api = "P7V+AiwPyztvIPr8EK+AlVWacCTa5FQWPNJs/0giVGt+0o985Srw35KwIYnIEPjqKgCVZomwbrcFt63vCUeUH9EPi2UwxqQ9XWraylX3/YHd/BPa/8W0wwZm36+XQ4LVuWAKhOopLbSrhHeprh9N7gdB04t89/1O/w1cDnyilFU=")
+
+line_bot_api = LineBotApi('P7V+AiwPyztvIPr8EK+AlVWacCTa5FQWPNJs/0giVGt+0o985Srw35KwIYnIEPjqKgCVZomwbrcFt63vCUeUH9EPi2UwxqQ9XWraylX3/YHd/BPa/8W0wwZm36+XQ4LVuWAKhOopLbSrhHeprh9N7gdB04t89/1O/w1cDnyilFU=')
+
 # Channel Secret
-handler = WebhookHandler("59e352af8b15a1efddee622ce3c31d81")
+
+handler = WebhookHandler('59e352af8b15a1efddee622ce3c31d81')
+
+
 
 # 監聽所有來自 /callback 的 Post Request
+
 @app.route("/callback", methods=['POST'])
+
 def callback():
+
     # get X-Line-Signature header value
+
     signature = request.headers['X-Line-Signature']
+
     # get request body as text
+
     body = request.get_data(as_text=True)
+
     app.logger.info("Request body: " + body)
+
     # handle webhook body
+
     try:
+
         handler.handle(body, signature)
+
     except InvalidSignatureError:
+
         abort(400)
+
     return 'OK'
 
+
+
 # 處理訊息
+
 @handler.add(MessageEvent, message=TextMessage)
+
 def handle_message(event):
-    print(event)
-    text=event.message.text
 
-    if (text=="Hi"):
-        reply_text = "Hello"
-        #Your user ID
+    message = TextSendMessage(text=event.message.text)
 
-    elif(text=="你好"):
-        reply_text = "哈囉"
-    elif(text=="機器人"):
-        reply_text = "叫我嗎"
-    else:
-        reply_text = text
-#如果非以上的選項，就會學你說話
-
-    message = TextSendMessage(reply_text)
     line_bot_api.reply_message(event.reply_token, message)
 
+
+
+@app.route('/')
+
+def index():
+
+    return 'Hello World'
+
+
+
 import os
+
 if __name__ == "__main__":
+
     port = int(os.environ.get('PORT', 5000))
+
     app.run(host='0.0.0.0', port=port)
