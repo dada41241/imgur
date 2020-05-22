563b5b6689ee8eac48b42ea1a8b0f9b29bab1e85
dada41241 <32788226+dada41241@users.noreply.github.com>
2020-05-22 21:41:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -103,7 +103,6 @@ def handle_message(event):
     
     if event.message.text == "Mariona":
         from random import choice
-        from scipy import *
         mottos2 = [1, 2, 3, 4, 5]
         line_bot_api.reply_message(
             event.reply_token, 
