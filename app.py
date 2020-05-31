f4d7c7c452f2abe0d9ca52d032f6865dd1ae02db
dada41241 <32788226+dada41241@users.noreply.github.com>
2020-05-31 22:39:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -118,16 +118,16 @@ def handle_message(event):
         return 0
     
     if event.message.text == "晚安":
-        client = ImgurClient('18f064544f219ac', 'b17f2b3ef24f98c4e3cce9424ef0b1b7173ef642')
-        images = client.get_album_images('k7Z38KG')
-        index = random.randint(0, len(images) - 1)
-        url = images[index].link
+
         message = TemplateSendMessage(
             alt_text='Carousel template',
             template=CarouselTemplate(
                 columns=[
                     CarouselColumn(
-
+                        client = ImgurClient('18f064544f219ac', 'b17f2b3ef24f98c4e3cce9424ef0b1b7173ef642')
+                        images = client.get_album_images('k7Z38KG')
+                        index = random.randint(0, len(images) - 1)
+                        url = images[index].link
                         thumbnail_image_url='url',
                         title='this is menu1',
                         text='description1',
