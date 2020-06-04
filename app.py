918197077c8fe4822891655578bd018c9e2de144
dada41241 <32788226+dada41241@users.noreply.github.com>
2020-06-04 22:11:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
-
 import requests
 import re
 import random
 from random import choice
+import csv
+
+
+  
+import gspread
+import random
+from oauth2client.service_account import ServiceAccountCredentials
+from pprint import pprint
+
+scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+
+creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
+
+client = gspread.authorize(creds)
+
+sheet = client.open("mottomorning").sheet1
+data=sheet.get_all_records()        
+        
+       
+
+        
 import configparser
 from bs4 import BeautifulSoup
+
 
 from imgurpython import ImgurClient
 
@@ -35,11 +56,11 @@ config.read("config.ini")
 
 # Channel Access Token
 
-line_bot_api = LineBotApi('P7V+AiwPyztvIPr8EK+AlVWacCTa5FQWPNJs/0giVGt+0o985Srw35KwIYnIEPjqKgCVZomwbrcFt63vCUeUH9EPi2UwxqQ9XWraylX3/YHd/BPa/8W0wwZm36+XQ4LVuWAKhOopLbSrhHeprh9N7gdB04t89/1O/w1cDnyilFU=')
+line_bot_api = LineBotApi('E9zSBfdSPaI8E76QVVHWp8slQvhT+H/5DlVp5bgwGZGksMV9A5fQFJ1W6sa0dHUDpQ4m11+DXMFppMIzmv02ze/uNf1GICPGvWEXhUbrv86hFIijndwfusSKIfRKXN7BzPlr/Zl8aYur9xl96uUjqwdB04t89/1O/w1cDnyilFU=')
 
 # Channel Secret
 
-handler = WebhookHandler('59e352af8b15a1efddee622ce3c31d81')
+handler = WebhookHandler('b56c198b7f81f9a15b1c33efcbeac68b')
 
 client = ImgurClient('18f064544f219ac', 'b17f2b3ef24f98c4e3cce9424ef0b1b7173ef642')
 
@@ -49,6 +70,19 @@ client = ImgurClient('18f064544f219ac', 'b17f2b3ef24f98c4e3cce9424ef0b1b7173ef64
 # 監聽所有來自 /callback 的 Post Request
 
 @app.route("/callback", methods=['POST'])
+
+def ettoday():
+    target_url = 'https://www.ettoday.net/news/realtime-hot.htm'
+    rs = requests.session()
+    res = rs.get(target_url, verify=False)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    content = ""
+    for data in soup.select('div.part_pictxt_3 div.piece.clearfix h3 a'):
+        title = data.text
+        link = data['href']
+        content += '{}\n{}\n\n'.format(title, link)
+    return content
+    
 
 def callback():
 
@@ -79,19 +113,6 @@ def callback():
 # 處理訊息
 
 @handler.add(MessageEvent, message=TextMessage)
-def ettoday():
-    target_url = 'https://www.ettoday.net/news/realtime-hot.htm'
-    print('Start parsing ptt hot....')
-    rs = requests.session()
-    res = rs.get(target_url, verify=False)
-    soup = BeautifulSoup(res.text, 'html.parser')
-    content = ""
-    for data in soup.select('div.part_pictxt_3 div.piece.clearfix h3 a'):
-        title = data.text
-        link = data['href']
-        print(title)
-        content += '{}\n{}\n\n'.format(title, link)
-    return content
     
 def handle_message(event):
     print("event.reply_token:", event.reply_token)
@@ -103,83 +124,10 @@ def handle_message(event):
             event.reply_token,
             TextSendMessage(text=content))
         return 0
-    
-    
-    if event.message.text == "午安":
-        client = ImgurClient('18f064544f219ac', 'b17f2b3ef24f98c4e3cce9424ef0b1b7173ef642')
-        images = client.get_album_images('k7Z38KG')
-        index = random.randint(0, len(images) - 1)
-        url = images[index].link
-        image_message1 = ImageSendMessage(
-            original_content_url=url,
-            preview_image_url=url
-        )
-        index = random.randint(0, len(images) - 1)
-        url = images[index].link
-        image_message2 = ImageSendMessage(
-            original_content_url=url,
-            preview_image_url=url
-        )
-        line_bot_api.reply_message(
-            event.reply_token, [image_message1, image_message2])
-        
-        
-                                                        
-        return 0
-    
-    
- 
-    
-    
-    
-    
-    
-    if event.message.text == "Mariona":
-        message = ImagemapSendMessage(
-            base_url='https://i.imgur.com/Y3Ld67O.png',
-            alt_text='this is an imagemap',
-            base_size=BaseSize(height=1040, width=1040),
-            actions=[
-                MessageImagemapAction(
-                    text='早安',
-                    area=ImagemapArea(
-                        x=0, y=0, width=520, height=520
-                    )
-                ),
-                MessageImagemapAction(
-                    text='午安',
-                    area=ImagemapArea(
-                        x=520, y=0, width=520, height=520
-                    )
-                ),
-                MessageImagemapAction(
-                    text='晚安',
-                    area=ImagemapArea(
-                        x=0, y=520, width=520, height=520
-                    )
-                ),
-                MessageImagemapAction(
-                    text='週末',
-                    area=ImagemapArea(
-                        x=520, y=520, width=520, height=520
-                    )
-                )
-            ]
-        )
-        line_bot_api.reply_message(event.reply_token, message)
-        
-        
-    if event.message.text == "觸電網-youtube":
-        target_url = 'https://www.youtube.com/user/truemovie1/videos'
-        rs = requests.session()
-        res = rs.get(target_url, verify=False)
-        soup = BeautifulSoup(res.text, 'html.parser')
-        seqs = ['https://www.youtube.com{}'.format(data.find('a')['href']) for data in soup.select('.yt-lockup-title')]
-        line_bot_api.reply_message(
-            event.reply_token, [
-                TextSendMessage(text=seqs[random.randint(0, len(seqs) - 1)]),
-                TextSendMessage(text=seqs[random.randint(0, len(seqs) - 1)])
 
+    
+        
+ 
 
 @app.route('/')
 
